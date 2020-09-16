dd51268e327d7a1eb9f3279d048fde4aadc51818
yellowsunn <chohankook95@gmail.com>
2020-09-16 16:34:24 +0900

    회원 수정 API

--- src/main/java/jpabook/jpashop/api/MemberApiController.java
+++ src/main/java/jpabook/jpashop/api/MemberApiController.java
@@ -37,6 +37,18 @@ public class MemberApiController {
         return new CreateMemberResponse(id);
     }
 
+    /**
+     * 수정 API
+     */
+    @PutMapping("/api/v2/members/{id}")
+    public UpdateMemberResponse updateMemberV2(@PathVariable("id") Long id,
+                                               @RequestBody @Valid UpdateMemberRequest request) {
+        memberService.update(id, request.getName());
+        Member findMember = memberService.findOne(id);
+
+        return new UpdateMemberResponse(findMember.getId(), findMember.getName());
+    }
+
     @Data
     private static class CreateMemberRequest {
         private String name;
@@ -46,5 +58,18 @@ public class MemberApiController {
     @AllArgsConstructor
     private static class CreateMemberResponse {
         private Long id;
+    }
+
+    @Data
+    private static class UpdateMemberRequest {
+
+        private String name;
+    }
+
+    @Data
+    @AllArgsConstructor
+    private static class UpdateMemberResponse {
+        private Long id;
+        private String name;
     }
 }

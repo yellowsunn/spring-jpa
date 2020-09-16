866587cd58a27b6782134e9575a1bd93860082b0
yellowsunn <chohankook95@gmail.com>
2020-09-16 22:54:37 +0900

    간단한 주문 조회 V2: 엔티티를 DTO로 변환

--- src/main/java/jpabook/jpashop/api/OrderSimpleApiController.java
+++ src/main/java/jpabook/jpashop/api/OrderSimpleApiController.java
@@ -1,14 +1,17 @@
 package jpabook.jpashop.api;
 
-import jpabook.jpashop.OrderSearch;
+import jpabook.jpashop.domain.Address;
 import jpabook.jpashop.domain.Order;
-import jpabook.jpashop.domain.OrderItem;
+import jpabook.jpashop.domain.OrderStatus;
 import jpabook.jpashop.repository.OrderRepository;
+import lombok.Data;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDateTime;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequiredArgsConstructor
@@ -16,13 +19,47 @@ public class OrderSimpleApiController {
 
     private final OrderRepository orderRepository;
 
+    /**
+     * v1. 엔티티 직접 노출
+     */
     @GetMapping("/api/v1/simple-orders")
     public List<Order> ordersV1() {
         List<Order> all = orderRepository.findAll();
-////        all.forEach(order -> {
-////            order.getMember().getName(); //Lazy 강제 초기화
-////            order.getDelivery().getAddress(); //Lazy 강제 초기화
-////        });
+//        all.forEach(order -> {
+//            order.getMember().getName(); //Lazy 강제 초기화
+//            order.getDelivery().getAddress(); //Lazy 강제 초기화
+//        });
         return all;
+    }
+
+    /**
+     * v2. 엔티티를 조회해서 DTO로 변환 (fetch join 사용 X)
+     */
+    @GetMapping("/api/v2/simple-orders")
+    public List<SimpleOrderDto> ordersV2() {
+        List<Order> orders = orderRepository.findAll();
+        List<SimpleOrderDto> result = orders.stream()
+                .map(SimpleOrderDto::new)
+                .collect(Collectors.toList());
+
+        return result;
+    }
+
+    @Data
+    private static class SimpleOrderDto {
+
+        private Long orderId;
+        private String name;
+        private LocalDateTime orderDate; //주문시간
+        private OrderStatus orderStatus;
+        private Address address;
+
+        public SimpleOrderDto(Order order) {
+            orderId = order.getId();
+            name = order.getMember().getName();
+            orderDate = order.getOrderDate();
+            orderStatus = order.getStatus();
+            address = order.getDelivery().getAddress();
+        }
     }
 }

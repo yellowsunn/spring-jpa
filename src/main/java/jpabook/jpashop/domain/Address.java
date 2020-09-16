d687ed016f064d87f346e22e42fb7289986f8cb1
yellowsunn <chohankook95@gmail.com>
2020-09-16 15:06:05 +0900

    JPA 기본 상태

---
+++ src/main/java/jpabook/jpashop/domain/Address.java
@@ -0,0 +1,19 @@
+package jpabook.jpashop.domain;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Embeddable;
+
+@Embeddable
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Getter
+public class Address {
+
+    private String city;
+    private String street;
+    private String zipcode;
+}

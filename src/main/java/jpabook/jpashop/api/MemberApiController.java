ad2b70ab202a54f6cd23918d663fedb59485a8f4
yellowsunn <chohankook95@gmail.com>
2020-09-16 15:34:09 +0900

    회원등록 API - v1 엔티티를 Request Body에 직접 매핑

---
+++ src/main/java/jpabook/jpashop/api/MemberApiController.java
@@ -0,0 +1,35 @@
+package jpabook.jpashop.api;
+
+import jpabook.jpashop.domain.Member;
+import jpabook.jpashop.service.MemberService;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
+
+@RestController
+@RequiredArgsConstructor
+public class MemberApiController {
+
+    private final MemberService memberService;
+
+    /**
+     * 등록 V1: 요청 값으로 Member 엔티티를 직접 받는다.
+     */
+    @PostMapping("/api/v1/members")
+    public CreateMemberResponse saveMemberV1(@RequestBody @Valid Member member) {
+        Long id = memberService.join(member);
+        return new CreateMemberResponse(id);
+    }
+
+    @Data
+    @AllArgsConstructor
+    private static class CreateMemberResponse {
+        private Long id;
+    }
+}

78c2651426128f0b742defac2c6f4f71a0f98794
yellowsunn <chohankook95@gmail.com>
2020-09-16 17:45:19 +0900

    회원 조회 API - V2 응답 값으로 엔티티가 아닌 별도의 DTO 사용

--- src/main/java/jpabook/jpashop/api/MemberApiController.java
+++ src/main/java/jpabook/jpashop/api/MemberApiController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequiredArgsConstructor
@@ -58,6 +59,17 @@ public class MemberApiController {
         return memberService.findMembers();
     }
 
+    @GetMapping("/api/v2/members")
+    public Result membersV2() {
+        List<Member> findMembers = memberService.findMembers();
+
+        List<MemberDto> collect = findMembers.stream()
+                .map(m -> new MemberDto(m.getName()))
+                .collect(Collectors.toList());
+
+        return new Result(collect.size(), collect);
+    }
+
     @Data
     private static class CreateMemberRequest {
         private String name;
@@ -80,5 +92,18 @@ public class MemberApiController {
     private static class UpdateMemberResponse {
         private Long id;
         private String name;
+    }
+
+    @Data
+    @AllArgsConstructor
+    private static class MemberDto {
+        private String name;
+    }
+
+    @Data
+    @AllArgsConstructor
+    private static class Result<T> {
+        private int count;
+        private T data;
     }
 }
